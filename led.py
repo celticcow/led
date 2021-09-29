7f0ff66b31d750ea1915bb3dbc57383f6c588965
celticcow <gregory.dunlap@gmail.com>
2021-09-28 20:27:16 -0500

    basic gpio sigle light

--- led.py
+++ led.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 
+import time
 import RPi.GPIO as GPIO
+
+def main():
+    green_led_pin = 18
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(green_led_pin, GPIO.OUT)
+    
+    GPIO.output(green_led_pin, True)
+    for i in range(10):
+        print("-", end=" ")
+        time.sleep(1)
+    #time.sleep(20)
+    GPIO.output(green_led_pin, False)
+
+if __name__ == "__main__":
+    main()
+#end of program
